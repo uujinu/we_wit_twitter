c993a3af5448ff715b83b54bcaa45e48acff6a84
youjin Cho <ud071233@gmail.com>
2023-02-11 23:06:50 +0900

    파일 경로를 절대경로로 변경

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
 
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} We-wit-Twitter</footer>
+    </>
+  );
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Routes>
