00472ecaeac9a92abb25f33528a95a58dda1cc7f
youjin Cho <ud071233@gmail.com>
2023-02-14 19:11:02 +0900

    로그인 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} We-wit-Twitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,69 @@
-import React from "react";
+import { authService } from "fbase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
 
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (e) => {
+    const {
+      target: { name, value }
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create newAccount
+        data = await createUserWithEmailAndPassword(authService, email, password);
+      } else {
+        // log in
+        data = await signInWithEmailAndPassword(authService, email, password)
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 
 export default Auth;
