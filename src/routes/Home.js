21f0c6ef917de07893cc53d71582ca16369c3f33
youjin Cho <ud071233@gmail.com>
2023-02-15 15:48:28 +0900

    트윗 등록 기능 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useState } from "react";
 
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [twit, setTwit] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange = (e) => {
+    e.preventDefault();
+    const {
+      target: { value }
+    } = e;
+    setTwit(value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={twit}
+        onChange={onChange}
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type="submit" value="Twit" />
+    </form>
+  );
+};
 
 
 export default Home;
