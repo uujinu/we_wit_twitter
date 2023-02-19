48f35a2f4d54d03929d1d15b2ccf152f4fbc5af3
youjin Cho <ud071233@gmail.com>
2023-02-20 01:13:00 +0900

    트윗 조회 기능 생성

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
@@ -15,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 
 export const authService = getAuth(app);
+export const dbService = getFirestore(app);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { dbService } from "fbase";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [twit, setTwit] = useState("");
-  const onSubmit = (e) => {
+  const [twits, setTwits] = useState([]);
+  
+  useEffect(() => {
+    onSnapshot(collection(dbService, "wwtwitter"), (snap) => {
+      snap.forEach(doc => {
+        const twtObj = { ...doc.data(), id: doc.id };
+        setTwits((prev) => [twtObj, ...prev]);
+      });
+    });
+  }, []);
+  
+  const onSubmit = async (e) => {
     e.preventDefault();
+    await addDoc(collection(dbService, "wwtwitter"), {
+      text: twit,
+      createdAt: Date.now(),
+      creatorId: userObj.uid
+    });
+    setTwit("");
   };
+
   const onChange = (e) => {
     e.preventDefault();
     const {
@@ -16,16 +36,25 @@ const Home = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        value={twit}
-        onChange={onChange}
-        type="text"
-        placeholder="What's on your mind?"
-        maxLength={120}
-      />
-      <input type="submit" value="Twit" />
-    </form>
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={twit}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Twit" />
+      </form>
+      <div>
+        {twits.map((twt) => (
+          <div key={twt.id}>
+            <h4>{twt.text}</h4>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
